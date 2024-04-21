a8e3647a49f5343b5ba37f6771eb7e4a6cce5b38
INTRINSEC\ASH <anis.halleb@cloud-temple.com>
2024-04-21 16:15:29 +0200

    finish the design part by adding message container and messages components

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,11 @@
     import React from 'react'
 import SearchInput from './SearchInput'
 import Conversations from './Coversations'
+import LogoutButton from './LogoutButton'
     
     const Sidebar = () => {
       return (
-        <div>
+        <div className=' border-r border-slate-500 p-4 flex flex-col'>
             <SearchInput />
             <div className="divider px-3"></div>
             <Conversations />
